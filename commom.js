cd189fdd58c74cdb6f1550134e91eb8d6c26c46e
Lightzin4521 <110855787+Lightzin4521@users.noreply.github.com>
2024-11-29 07:55:24 -0300

    Add files via upload

--- commom.js
+++ commom.js
@@ -9,4 +9,25 @@ const getCSS = (variavel) => {
         family: getCSS("--font")
         }
         
-        export {getCSS, tickConfig}
+function criarGrafico(data, layout){
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container') .appendChild(grafico)
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout)
+}
+export {getCSS, tickConfig, criarGrafico}
+
+function incluirTexto (texto) {
+    const container = document.getElementById
+    ('graficos-container');
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+}
+
+export {getCSS, tickConfig, criarGrafico, incluirTexto}
--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -27,11 +27,7 @@ const data = [
     }
 }
 ]
-    const grafico = document.createElement("div")
-    grafico.className = "grafico"
 
-    document.getElementById("graficos-container").appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
 
 quantidadeUsuarios()
 const layout = {
